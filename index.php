712dfdef2bf59f3b51f69eae2493f2209e8d7c1f
mikicola <michela.colangelo@gmail.com>
2022-04-14 16:10:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+    $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ipsa debitis numquam optio tempore odit veniam voluptate corrupti pariatur alias dolore quos, quam doloribus quo earum officia eos aut! Officia!';
+    
+    /*
+    stampo il testo con echo
+    i . servono a concatenare
+    strlen stampa la lunghezza
+    */
+    echo $text . 'la lunghezza del testo è: ' . strlen($text);
+    
+    ?>
+
+</body>
+</html>
